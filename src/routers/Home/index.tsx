0dfd2e076e17b8804b1ce549df168c54e204a301
jandou <809781809@qq.com>
2018-07-02 00:27:49 +0800

    feat: add Ant UI

--- src/routers/Home/index.tsx
+++ src/routers/Home/index.tsx
@@ -2,17 +2,14 @@ import * as React from 'react';
 import { hot } from 'react-hot-loader';
 import * as CSSModules from 'react-css-modules';
 const style = require('./style.scss');
-import { Link } from 'react-router-dom';
-const img_path = require('../../assets/react.jpg');
+import PageCommonComponent from '../../component/common';
+
 @CSSModules(style, {allowMultiple: true})
 class HomeComponent extends React.Component {
   render() {
     return (
       <div styleName="home-page">
-        <h1>TypeScript SPA.</h1>
-        <p>This is a demo page.</p>
-        <p>You can also click<Link to={'/about'}> about page</Link> to try react-router.</p>
-        <img src={img_path} />
+        <PageCommonComponent/>
       </div>
     );
   }
--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import 'antd/dist/antd.css';
 import {
   BrowserRouter as Router,
   Route,
